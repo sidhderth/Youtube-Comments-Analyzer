09ac461b9e7f72cbf43d8390bf78ee8519731154
samridh20 <samridhsst@gmail.com>
2024-10-13 10:45:17 -0500

    added app.py to simplify the code into one file
    added app.py to simplify the code into one file

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -5,8 +5,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         // Get the video ID from the current URL
         const videoId = window.location.search.split('v=')[1].split('&')[0];
 
-        chrome.runtime.sendMessage({ action: "videoIdScraped", data: videoId });
+        // Send the video ID to a local server
+        fetch("http://localhost:5000/video_id", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ videoId: videoId }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Video ID sent to server:", videoId);
+            sendResponse({ success: true });
+        })
+        .catch(error => {
+            console.error("Error sending video ID to server:", error);
+            sendResponse({ success: false });
+        });
 
-        sendResponse({ success: true });
+        return true; // Keep the message channel open for sendResponse
     }
 });
